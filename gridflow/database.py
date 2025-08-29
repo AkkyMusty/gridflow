514c86e3193e66f894041cd77f554e8d811b97e2
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-08-29 23:43:54 +0200

    Database schema created and user route created

--- gridflow/database.py
+++ gridflow/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 import os
 from dotenv import load_dotenv
 
@@ -12,3 +12,11 @@ engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency for FastAPI
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
